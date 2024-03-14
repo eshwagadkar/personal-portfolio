d28e3eb6ec9a0c908bf3140300d509b90dd4a5bd
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-14 16:12:54 +0530

    import the proper path of style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import '../styles/main.scss';
+import './assets/styles/main.scss';
+
+if( process.env.NODE_ENV === 'production' ) {
+    console.log('Production Mode')
+} else if ( process.env.NODE_ENV === 'development' ) {
+    console.log('Development Mode')
+}
 
 if(module.hot) {
     module.hot.accept()
