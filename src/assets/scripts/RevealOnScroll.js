4d98363662f69080a2dd1e9c6973d1890722f31b
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-08 14:56:27 +0530

    throttle the onscroll event

--- src/assets/scripts/RevealOnScroll.js
+++ src/assets/scripts/RevealOnScroll.js
@@ -1,33 +1,31 @@
+import throttle from "lodash/throttle"
+
 class RevealOnScroll {
     
     constructor() {
-        this.itemsToReveal = document.querySelectorAll('.skill-item')
-        this.itemsToRevealProjects = document.querySelectorAll('.projects')
-        this.hideInitially()
-        this.events()
+      this.itemsToReveal = document.querySelectorAll('.skill-item')
+      this.itemsToRevealProjects = document.querySelectorAll('.projects')
+      this.hideInitially()
+      this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+      this.events()
     }
 
+    events() { window.addEventListener('scroll', this.scrollThrottle) }
+
+    calcCaller() {
+        console.log('Scroll function ran')
+
+        this.itemsToReveal.forEach(el => this.calculateIfScrolledTo(el) )
+        this.itemsToRevealProjects.forEach(el => this.calculateIfScrolledTo(el) )
+    }
+
+    
     calculateIfScrolledTo(el){
         let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100
 
         if(scrollPercent < 85) {
             el.classList.add('reveal-item--is-visible')
         }
-    }
-
-    events() {
-        window.addEventListener('scroll', () => {
-            
-            this.itemsToReveal.forEach(el => {
-            this.calculateIfScrolledTo(el)
-            })
-
-            this.itemsToRevealProjects.forEach(el => {
-            this.calculateIfScrolledTo(el)
-            })
-
-
-        })
     }
 
     hideInitially() {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import RevealOnScroll from './assets/scripts/RevealOnScroll';
 
 let revealOnScroll = new RevealOnScroll()
 
+
 if( process.env.NODE_ENV === 'production' ) {
     console.log('Production Mode')
 } else if ( process.env.NODE_ENV === 'development' ) {
