232c6d9c3835f1d78439c8969578170ba09f0c73
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-08 17:48:53 +0530

    optimizing the throttle functionality

--- src/assets/scripts/RevealOnScroll.js
+++ src/assets/scripts/RevealOnScroll.js
@@ -13,25 +13,50 @@ class RevealOnScroll {
     events() { window.addEventListener('scroll', this.scrollThrottle) }
 
     calcCaller() {
-        console.log('Scroll function ran')
-
-        this.itemsToReveal.forEach(el => this.calculateIfScrolledTo(el) )
-        this.itemsToRevealProjects.forEach(el => this.calculateIfScrolledTo(el) )
+        // console.log('Scroll Function Ran')
+        this.itemsToReveal.forEach(el => {
+            if(el.isRevealed ==  false) {
+                this.calculateIfScrolledTo(el)
+            }
+        })
+        
+        this.itemsToRevealProjects.forEach(el => {
+            if(el.isRevealed === false) {
+                this.calculateIfScrolledTo(el)
+            }
+        })
     }
 
     
     calculateIfScrolledTo(el){
+        // console.log('Element was calculated')
         let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100
 
-        if(scrollPercent < 85) {
+        if(scrollPercent < 75) {
             el.classList.add('reveal-item--is-visible')
+            el.isRevealed = true
+
+            if(el.isLastItem) { 
+                window.removeEventListener('scroll', this.scrollThrottle)
+            }
         }
     }
 
     hideInitially() {
-        this.itemsToReveal.forEach(el => el.classList.add('reveal-item'))
-        this.itemsToRevealProjects.forEach(el => el.classList.add('reveal-item'))
-        
+
+        this.itemsToReveal.forEach(el => { 
+            el.classList.add('reveal-item')
+            el.isRevealed = false
+        })
+
+        this.itemsToRevealProjects.forEach(el => {
+            el.classList.add('reveal-item')
+            el.isRevealed = false
+        })
+
+        // To find the last item in nodelist and add a property (isLastItem) to it.
+        this.itemsToRevealProjects[this.itemsToRevealProjects.length -1].isLastItem = true 
+
     }
 }
 
